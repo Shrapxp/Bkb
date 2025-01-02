3f3de173559330b4a12ec25c991f922fc65fc1b6
Shrapxp <shrap912@gmail.com>
2025-01-02 23:56:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7955245382:AAHS62duTTlDYlcIzIQ7AuwlDpj4rMt1rmE")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RitikXyz099")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6437994839")
+
+
+
+
+
+
+
+  
