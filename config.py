932491a23db23f4cfa2fbf6df09fae2983c28edd
Shrapxp <shrap912@gmail.com>
2025-01-02 23:57:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7955245382:AAHS62duTTlDYlcIzIQ7AuwlDpj4rMt1rmE")
+BOT_TOKEN=("7630314402:AAH9szViee1jmN0kUlLOS1IUnRww7xU5rRc")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@RitikXyz099")
+OWNER_USERNAME=("@rajaraj_04")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6437994839")
+ADMIN_IDS=("7855020275")
 
 
 
